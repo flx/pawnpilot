a14cba7cf4aac446b59face0ffdd0b00444a1c72
Felix Matschke <felix.matschke@icloud.com>
2025-12-23 06:23:54 -0500

    Initial Commit

---
+++ PawnPilot/PawnPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawnPilotApp.swift
+//  PawnPilot
+//
+//  Created by Felix Matschke on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PawnPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
